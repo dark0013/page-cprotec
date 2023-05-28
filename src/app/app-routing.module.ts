d24764cfc3c8b98b8d95d73c3599e18f31742dbd
Alain Campoverde <andresarcangel00@gmail.com>
2023-05-28 18:11:36 -0500

    se agrego las imagenes, css y todo lo relacionado con contactanos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactanosComponent } from './page/contactanos/contactanos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'page/contactanos', component: ContactanosComponent
+  },
+  /*{
+    path: '', component: ContactanosComponent
+  }*/
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
