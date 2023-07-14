8683d7662f2822acc1e81023b9fda806b97d9c1f
Alain Campoverde <andresarcangel00@gmail.com>
2023-07-14 00:54:27 -0500

    se acomodo la parte de nosotros

--- src/app/page/nosotros/nosotros.component.ts
+++ src/app/page/nosotros/nosotros.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nosotros',
   templateUrl: './nosotros.component.html',
-  styleUrls: ['./nosotros.component.css']
+  styleUrls: ['./nosotros.component.css'],
 })
-export class NosotrosComponent {
-
+export class NosotrosComponent implements OnInit {
+  imagenes: string[] = [
+    'assets/img/Logo convenios y aliados/C_ITSGUAYAQUIL_.png',
+  ];
+  imagenesCliente: string[] = [
+    'assets/img/Logo convenios y aliados/CL_LIS_FASHION.png',
+    'assets/img/Logo convenios y aliados/CL_MR.COMPU.png',
+    'assets/img/Logo convenios y aliados/CL_Consorcio-Pichincha.png',
+  ];
+  ngOnInit() {}
 }
